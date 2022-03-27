ca4222eef12df1d261f796cecff79209479ddc45
Aissam4 <aissambarchil75@gmail.com>
2022-03-27 21:59:08 +0000

    trying to fetch data from api

--- CardDuplicate.js
+++ CardDuplicate.js
@@ -14,17 +14,17 @@ var swiper = new Swiper(".mySwiper", {
 	},
 });
 
-// for (var i = 0; i < 10; i++)
-// {
-// 	fetch(`https://api.nftport.xyz/v0/nfts/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb/${i}?chain=ethereum`, {
-// 		"method": "GET",
-// 		"headers": {
-// 			"Content-Type": "application/json",
-// 			"Authorization": "f636f122-48b6-45ec-942e-1415d9eda95b"
-// 		}
-// 	})
-// 	.then(res => res.json())
-// 	.then((out) => {
-// 		console.log('Output: ', out);
-// 	}).catch(err => console.error(err));
-// }
+for (var i = 0; i < 10; i++)
+{
+	fetch(`https://api.nftport.xyz/v0/nfts/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb/${i}?chain=ethereum`, {
+		"method": "GET",
+		"headers": {
+			"Content-Type": "application/json",
+			"Authorization": "f636f122-48b6-45ec-942e-1415d9eda95b"
+		}
+	})
+	.then(res => res.json())
+	.then((out) => {
+		console.log('Output: ', out);
+	}).catch(err => console.error(err));
+}
