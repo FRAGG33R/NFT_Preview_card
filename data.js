80e2bb8c5e4c5433e91e5804ac1bd7a0eceb5e21
Aissam Barchil <abarchil@e1r2p5.1337.ma>
2022-04-02 22:46:43 +0000

    bug requeste api

--- data.js
+++ data.js
@@ -22,12 +22,39 @@ const req = http.request(options, (res) => {
     const body = Buffer.concat(chunks);
     let result = body.toString();
     result = JSON.parse(result);
-    for(let i = 0; i < 10; i++)
+    for(let i = 0; i < 100; i++)
     {
-        let isnum = /^\d+$/.test(result.nfts[i].token_id);
+      let isnum = /^\d+$/.test(result.nfts[i].token_id);
         if (isnum)
         {
-            //send request
+        console.log(result.nfts[i].token_id);
+          const https = require("https");
+            const option = {
+              "method": "GET",
+              "hostname": "api.nftport.xyz",
+              "port": null,
+              "path": `/v0/nfts/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/${result.nfts[i].token_id}?chain=ethereum`,
+              "headers": {
+                "Content-Type": "application/json",
+                "Authorization": "f636f122-48b6-45ec-942e-1415d9eda95b"
+              }
+          };
+
+          const req_ = https.request(option, function (res) {
+            const chunks = [];
+
+            res.on("data", function (chunk) {
+              chunks.push(chunk);
+            });
+
+            res.on("end", function () {
+              const body = Buffer.concat(chunks);
+              let result = body.toString();
+              result = JSON.parse(result);
+              console.log(result);
+            });
+            }); 
+              req_.end();
         }
     }
 });
